9fb9b302b4cb6f221a2aea50d81d03032ccf6647
madason911 <31772534+madason911@users.noreply.github.com>
2017-12-12 15:04:19 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,42 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
- #include <malloc.h>
 
- int qq = 0;
- int stack[1000];
+
+ int n = 0;
+ /*create a stack of 100 elements*/
+ int stack[100];
  int pop(void) {
-     if (qq > 0) {
-          return stack[qq--];
+     if (n > 0) {
+          return stack[--n];
      } else {
           printf("ERROR\n");
           return 0;
      }
  };
  void push(int a) {
-     stack[qq++] = a;
+     stack[n++] = a;
  };
  int empty() {
-     return (qq == 0);
+     return (n == 0);
  }
 
  int main() {
      int i;
+  /* create an infinite loop*/
      while (1>0) {
          int znak = getchar();
          int x;
          switch (znak) {
              case '\n':
-             case ' ' : break;
-             case '=' : printf("Result = %d\n", pop()); break;
-             case 27  : goto RESULT;
-             case '+' : push(pop() + pop()); break;
-             case '-' : push(-pop() + pop()); break;
-             case '*' : push(pop() * pop()); break;
-             case '/' : push(pop() / pop()); break;
+          case ' ' : break;
+             case 1 : goto RESULT;
+             case '+' : 
+           push(pop() + pop()); break;
+             case '-' : 
+           push(-pop() + pop()); break;
+             case '*' : 
+           push(pop() * pop()); break;
+             case '=' : 
+           printf("Result = %d\n", pop()); break;
+             case 1 : 
+           goto RESULT;
              default:
                  ungetc(znak, stdin);
                  if (scanf("%d", &x) != 1) {
-                     fprintf(stderr, "Can't read integer\n");
+                     fprintf("ERROR\n");
                      return -1;
                  } else {
                      push(x);
